af62b392c86392fa96f4c6268f41d14fa9d91c1a
qiyubrother <qiyubrother@outlook.com>
2019-10-30 11:41:20 +0800

    添加项目文件。

---
+++ CSharpCallVCdll/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace CSharpCallVCdll
+{
+    class Program
+    {
+        [DllImport(@"C:\Users\qiyub\source\repos\CSharpCallVCdll\Debug\VCdll.dll")]
+        public static extern int Add(int a, int b);
+        static void Main(string[] args)
+        {
+            var a = 100;
+            var b = 200;
+            Console.WriteLine($"{a}+{b}={Add(a, b)}");
+        }
+    }
+}
---
+++ VCdll/pch.cpp
@@ -0,0 +1,11 @@
+﻿// pch.cpp: 与预编译标头对应的源文件
+
+#include "pch.h"
+
+// 当使用预编译的头时，需要使用此源文件，编译才能成功。
+
+
+// -- DLL导出函数导出声明区 --
+
+//extern "C" __declspec(dllexport) int Add(int a, int b);
+
